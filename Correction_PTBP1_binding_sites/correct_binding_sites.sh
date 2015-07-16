5e3d3b8dc74dc769889dd15335c0d84144a93264
Nejc Haberman <nejc.haberman@gmail.com>
2015-07-16 15:03:08 +0100

    minor correction in bash script

--- Correction_PTBP1_binding_sites/correct_binding_sites.sh
+++ Correction_PTBP1_binding_sites/correct_binding_sites.sh
@@ -16,8 +16,8 @@
 #
 # BEDtools software package needs to be installed first: 
 # http://bedtools.readthedocs.org/en/latest/
-
-export PATH=/home/programs/bedtools-2.17.0/bin:$PATH
+# 
+# export PATH=/home/programs/bedtools-2.17.0/bin:$PATH
 
 clusters=$1
 complete_reads=$2
@@ -25,42 +25,47 @@ incomplete_reads=$3
 path=`pwd -P`
 
 # 1. remove all reads that don't overlap with cluster ends
-bedtools intersect -s -a  ${path}${complete_reads} -b  ${path}${clusters}.BED -wa >  ${path}${complete_reads}-candidates.bed
-rm ${path}${complete_reads}-End.BED
+bedtools intersect -s -a  ${path}/${complete_reads} -b  ${path}/${clusters} -wa >  ${path}/${complete_reads}-candidates.bed
+#rm ${path}/${complete_reads}-End.BED
 
 # 2. Find closest ends
-python getEnd-BED.py ${path}${complete_reads}-candidates.bed ${path}${complete_reads}-candidates-End.bed
-bedtools closest -s -D a -io -iu -t first -a ${path}${clusters}.BED -b ${path}${complete_reads}-candidates-End.bed > ${path}${clusters}-closest-iCLIP-end.BED
-rm ${path}${complete_reads}-candidates.bed
-rm ${path}${complete_reads}-candidates-End.bed
+python getEnd-BED.py ${path}/${complete_reads}-candidates.bed ${path}/${complete_reads}-candidates-End.bed
+sort -k1,1 -k2,2n -k6,6 ${path}/${complete_reads}-candidates-End.bed > ${path}/${complete_reads}-candidates-End-sorted.bed
+mv ${path}/${complete_reads}-candidates-End-sorted.bed ${path}/${complete_reads}-candidates-End.bed
+bedtools closest -s -D a -io -iu -t first -a ${path}/${clusters} -b ${path}/${complete_reads}-candidates-End.bed > ${path}/${clusters}-closest-iCLIP-end.BED
+rm ${path}/${complete_reads}-candidates.bed
+rm ${path}/${complete_reads}-candidates-End.bed
 
 # 3. correct ends if they are and if they are less then 40 nts away which is the maximum read length 
-python correctClusterEnds.py ${path}${clusters}-closest-iCLIP-end.BED ${path}${clusters}-closest-iCLIP-end-corrected.bed
-rm ${path}${clusters}-closest-iCLIP-end.BED
+python correctClusterEnds.py ${path}/${clusters}-closest-iCLIP-end.BED ${path}/${clusters}-closest-iCLIP-end-corrected.bed
+rm ${path}/${clusters}-closest-iCLIP-end.BED
 
 # 4. Find candidates from untrimmed reads (no adapter) and then find the closest End to uncorrected clusters by using PTB-iiCLIP without adapter removal
-bedtools intersect -s -a ${path}${clusters} -b ${path}${incomplete_reads}.BED -wa > ${path}${incomplete_reads}-candidates.bed
-python getEnd-BED.py ${path}${incomplete_reads}-candidates.bed ${path}${incomplete_reads}-candidates-End.bed
-bedtools closest -s -D a -io -iu -t first -a ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed -b ${path}${incomplete_reads}-candidates-End.bed > ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest.bed
-rm ${path}${incomplete_reads}-candidates.bed
-rm ${path}${incomplete_reads}-candidates-End.bed
-rm ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed
+bedtools intersect -s -a ${path}/${incomplete_reads} -b ${path}/${clusters} -wa > ${path}/${incomplete_reads}-candidates.bed
+python getEnd-BED.py ${path}/${incomplete_reads}-candidates.bed ${path}/${incomplete_reads}-candidates-End.bed
+sort -k1,1 -k2,2n -k6,6 ${path}/${incomplete_reads}-candidates-End.bed > ${path}/${incomplete_reads}-candidates-End-sorted.bed
+mv ${path}/${incomplete_reads}-candidates-End-sorted.bed ${path}/${incomplete_reads}-candidates-End.bed
+bedtools closest -s -D a -io -iu -t first -a ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed -b ${path}/${incomplete_reads}-candidates-End.bed > ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest.bed
+rm ${path}/${incomplete_reads}-candidates.bed
+rm ${path}/${incomplete_reads}-candidates-End.bed
+rm ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed
 
 # 5. correct ends of no_adapters iCLIP reads if they are and if they are less then 40 nts away which is the maximum read length
-python correctClusterEnds.py  ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest.bed  ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed
+python correctClusterEnds.py  ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest.bed  ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed
 
 # 6. merge clusters together: corrected from both (adapter and no_adapter) and NOT corrected after the previous step
-cat ${path}${clusters}-closest-iCLIP-end-corrected.bed ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed-NOT.bed >  ${path}${clusters}-merged.bed
-rm ${path}${clusters}-closest-iCLIP-end-corrected.bed
-rm ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed
-rm ${path}${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed-NOT.bed
+cat ${path}/${clusters}-closest-iCLIP-end-corrected.bed ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed-NOT.bed >  ${path}/${clusters}-merged.bed
+rm ${path}/${clusters}-closest-iCLIP-end-corrected.bed
+rm ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest.bed
+rm ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed
+rm ${path}/${clusters}-closest-iCLIP-end-corrected.bed-NOT.bed-closest-corrected.bed-NOT.bed
 
 # 7. sort
-sort -k1,1 -k2,2n -k5,5 ${path}${clusters}-merged.bed > ${path}${clusters}-merged-sorted.bed
-rm ${path}${clusters}-merged.bed
+sort -k1,1 -k2,2n -k5,5 ${path}/${clusters}-merged.bed > ${path}/${clusters}-merged-sorted.bed
+rm ${path}/${clusters}-merged.bed
 
 # 8. merge clusters that are less then 20 nt apart (By selecting count >1 we can get all merged clusters)
-bedtools merge -i ${path}${clusters}-merged-sorted.bed -s -d 20 -c 5,5,6 -o distinct,count,distinct > ${path}${clusters}-corrected.bed
-rm ${path}${clusters}-merged-sorted.bed
+bedtools merge -i ${path}/${clusters}-merged-sorted.bed -s -d 20 -c 5,5,6 -o distinct,count,distinct > ${path}/${clusters}-corrected.bed
+rm ${path}/${clusters}-merged-sorted.bed
 
 
